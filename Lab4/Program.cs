19ae225d9f2891d2a13861fd7eda5b178aaabbf9
dowskee <31831031+dowskee@users.noreply.github.com>
2017-09-27 16:36:50 -0400

    Corrections to factorial calculations

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -9,27 +9,37 @@ namespace Lab4
     class Program
     {
         static void Main(string[] args)
+
         {
-            //design a program that prompts the user to enter an integer between 1 and 10
-
-            //input
-            int Input, response;
-
-            Console.WriteLine("Please enter an integer between 1 and 10:");
-            Input = int.Parse(Console.ReadLine()); //reads user's input and parses 
-
-            //processing
-            for (int i = Input; i >= 1; i--) ; // example: 5! = 5*4*3*2*1 (n! = n*(n-1)...*1)
-            response = (Input * Input); //*1 from comment above
+            bool Continue = true;
+            while (Continue == true)
             {
-                if (Input >= 1 && Input <= 10) //ensure integer is in range
-                { 
-                Console.WriteLine("The factorial of " + Input + " is " + response);
-                } 
-                else
-                
-                    Console.WriteLine("That is not a valid integer. Please run again."); //find a way to loop? Do while with for?
-               
+                //design a program that prompts the user to enter an integer between 1 and 10
+
+                //input
+                long i, Input, fact;
+
+                Console.WriteLine("Please enter an integer between 1 and 10:");
+                Input = long.Parse(Console.ReadLine()); //reads user's input and parses 
+                Console.WriteLine("You entered " + Input + ". The factorial of " + Input + " is:");
+                fact = Input;
+                //processing
+                for (i = Input - 1; i <= 1; i++)   // example: 5! = 5*4*3*2*1 (n! = n*(n-1)...*1)
+                {
+                    //output
+                    fact = (fact * i);
+                }
+                Console.WriteLine("The factorial of " + Input + " is: " + fact);
+
+
+
+                string Choice;
+                Console.WriteLine("Would you like to enter another number? Y/N?");
+                Choice = Console.ReadLine();
+                if (Choice == "n" || Choice == "N" || Choice == "no" || Choice == "No")
+                {
+                    Continue = false;
+                }
             }
         }
     }
